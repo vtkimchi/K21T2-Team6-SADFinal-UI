29a8113386955e5438af5f8205bd8cb36b2887c1
vothikimchi <vtkchi1297@gmail.com>
2018-06-07 11:35:11 +0700

    final release UI

--- Recruitement/App_Start/RouteConfig.cs
+++ Recruitement/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Recruitement
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Candidates", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
